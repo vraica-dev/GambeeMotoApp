970c2c21047adc702f1b27ef582056e365a4bacc
rvgvba <rvg.vali@gmail.com>
2021-03-04 12:35:11 +0200

    v1.0.0 -WIP

--- app.py
+++ app.py
@@ -16,5 +16,7 @@ def trips():
     return render_template('trips.html')
 
 
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port, debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
