ea670763de5368648ac44155ece8a2f68a04ab5b
rvgvba <rvg.vali@gmail.com>
2021-03-04 22:04:34 +0200

    v1.1.0 -WIP

--- application/routes.py
+++ application/routes.py
@@ -11,24 +11,26 @@ def home():
 
 @app.route('/trips', methods=['GET', 'POST'])
 def trips():
-
     if request.method == 'GET':
         return render_template('trips.html')
 
     if request.method == 'POST':
         temp_trip_name = request.form['trip_name']
         temp_area_name = request.form['area_name']
-        temp_km_travelled = request.form['km_travelled']
-        temp_hours_riding = request.form['hours_riding']
-        temp_km_initial = request.form['km_initial']
-        temp_km_final= request.form['km_final']
+        temp_km_travelled = int(request.form['km_travelled'])
+        temp_hours_riding = int(request.form['hours_riding'])
+        temp_km_initial = int(request.form['km_initial'])
+        temp_km_final = int(request.form['km_final'])
 
-        print(temp_trip_name)
-
-        new_trip = TripRecords(trip_name = temp_trip_name, area_name = temp_area_name, km_travelled =temp_km_travelled, h_travelled= temp_hours_riding)
+        new_trip = TripRecords(trip_name=temp_trip_name, area_name=temp_area_name, km_travelled=temp_km_travelled,
+                               h_travelled=temp_hours_riding, km_initial=temp_km_initial, km_final=temp_km_final)
         db.session.add(new_trip)
         db.session.commit()
 
-        print('done')
-
         return redirect(url_for('trips'))
+
+
+@app.route('/view_trips', methods=['GET'])
+def view_trips():
+    trips = TripRecords.query.all()
+    return render_template('view_trips.html', list_trips=trips)
